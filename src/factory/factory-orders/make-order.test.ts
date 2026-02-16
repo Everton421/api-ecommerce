177c06fa2b1a5bd6e39e674cc69caf029dbf96bc
everton421 <evertonjose421@gmail.com>
2026-02-16 18:00:29 -0300

    16022026

---
+++ src/factory/factory-orders/make-order.test.ts
@@ -0,0 +1,13 @@
+/*
+import test from "node:test";
+import { makeProduct } from "../factory-product/make-product.ts";
+
+
+test("[ Make order ]  ", async ()=>{
+    
+    const productId = await makeProduct();
+
+        console.log(productId);
+
+})
+*/
